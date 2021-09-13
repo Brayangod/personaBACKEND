b93db745bc3e5ae19945bde6465a3dad988b9ce7
Brayan Godoy <bggodoy@ulp.edu.ar>
2021-09-13 15:24:59 -0300

    Se soluciono error con la base de datos en las entidades

--- PersonaBACKEND/src/main/java/com/brayanproject/persona/model/Estado.java
+++ PersonaBACKEND/src/main/java/com/brayanproject/persona/model/Estado.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -13,6 +16,8 @@ public class Estado {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	@ManyToOne
+	@JoinColumn(name = "id_pais")
 	private Pais pais;
 	private String nombre;
 	public Estado(Pais pais, String nombre) {
